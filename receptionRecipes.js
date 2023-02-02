1888bb7ffccf2ee9cf0153ff50636a4e7ac43cd6
NicolasSarasin <108725344+NicolasSarasin@users.noreply.github.com>
2023-02-02 14:02:26 +0100

    Add files via upload

--- receptionRecipes.js
+++ receptionRecipes.js
@@ -1,55 +1,58 @@
 async function getData() {
-	const response = await fetch("recipes.js");
-	const data = await response.js();
-	return data;
+    const response = await fetch("recipes.js");
+    const data = await response.js();
+    return data;
 }
 
 async function getRecipesData() {
-	const recipers = (await getData()).recipers;
+    const recipers = (await getData()).recipers;
     console.log(recipers);
 
-    // et bien retourner le tableau photographers seulement une fois
+    // return recipers tab one time
     return [...recipers];
 }
 
-/*async function getReciperData(id) {
-
+/*async function getReciperData(id) {	
 }*/
 
-function algorithmic(){
-	const research1 = document.getElementById("ingredient");
-	const research2 = document.getElementById("devices");
-	const research3 = document.getElementById("utensils");
+function algorithmic() {
+    const research1 = document.getElementById("ingredient");
+    const research2 = document.getElementById("devices");
+    const research3 = document.getElementById("utensils");
+    if (research1) {
+    } else if (research2) {
+    } else {
+    }
 }
 
-function recipersFactory(data){
-	const{name,ingredient,quantity,unit}=data;
-	function getRecipersCardDOM() {
-		const displayResipers = document.createElement("article");
-		const generalDiv=document.createElement("div");
-		const reciperDiv=document.createElement("div");
-		const name=document.createElement("h1");
-		const titleReciper=document.createElement("h1");
-		const span=document.createElement("span");
-		generalDiv.classList.add("mainDiv");
-		reciperDiv.classList.add("bottomDiv");
-		name.textContent=name;
-		titleReciper.textContent=ingredient + ": ";
-		span.textContent=quantity + unit;
-		displayResipers.appendChild(generalDiv);
-		displayResipers.appendChild(reciperDiv);
-		reciperDiv.appendChild(name);
-		reciperDiv.appendChild(titleReciper);
-		reciperDiv.appendChild(span);
-		return displayResipers;
-	}
-	return{name, getRecipersCardDOM};
+function recipersFactory(data) {
+    const { name, ingredient, quantity, unit } = data;
+    function getRecipersCardDOM() {
+        const displayResipers = document.createElement("article");
+        const generalDiv = document.createElement("div");
+        const reciperDiv = document.createElement("div");
+        const name = document.createElement("h1");
+        const titleReciper = document.createElement("h1");
+        const span = document.createElement("span");
+        generalDiv.classList.add("mainDiv");
+        reciperDiv.classList.add("bottomDiv");
+        name.textContent = name;
+        titleReciper.textContent = ingredient + ": ";
+        span.textContent = quantity + unit;
+        displayResipers.appendChild(generalDiv);
+        displayResipers.appendChild(reciperDiv);
+        reciperDiv.appendChild(name);
+        reciperDiv.appendChild(titleReciper);
+        reciperDiv.appendChild(span);
+        return displayResipers;
+    }
+    return { name, getRecipersCardDOM };
 }
 
-async function displayRecipersData(){
-	const recipersSection = document.querySelector(".recipes");
-	recipersSection.innerHTML = "";
-	recipes.forEach((recipers) => {
+async function displayRecipersData() {
+    const recipersSection = document.querySelector(".recipes");
+    recipersSection.innerHTML = "";
+    recipes.forEach((recipers) => {
         const mediaModel = recipersFactory(recipes);
         const userRecipersCardDOM = mediaModel.getRecipersCardDOM();
         recipersSection.appendChild(userRecipersCardDOM);
@@ -57,6 +60,6 @@ async function displayRecipersData(){
 }
 
 async function init() {
-	// Récupère les datas
-	displayRecipersData();
+    // Récupère les datas
+    displayRecipersData();
 }
